a3611649538579b2c1b193a038b662c761721520
Willem van der Plaat <wplaat@gmail.com>
2016-06-28 23:14:56 +0200

    Change webcam script to use ffmpeg tool. Its more stable. No kernel panic so ware :)

--- src/interfaces/webcam.php
+++ src/interfaces/webcam.php
@@ -33,7 +33,7 @@ if( plaatprotect_islocked() ) die( "Already running.\n" );
 
 plaatprotect_db_connect($dbhost, $dbuser, $dbpass, $dbname);
 
-$detect_level=15;
+$detect_level=35;
 $detect_areas=25;
 $im2 = '';
 
@@ -50,22 +50,29 @@ function getColor($img, $x, $y) {
     return array ($x, $y, $r, $g, $b);
 }
 
-function plaatprotect_make_picture() {
+function plaatprotect_make_picture($name, $width, $height) {
 
    $path = BASE_DIR.'/webcam/'.date('Y-m-d');		
    plaatprotect_create_path($path);
 	
-   $source = BASE_DIR.'/webcam/image1.jpg';
+	#$source = BASE_DIR.'/webcam/image1.jpg';
+   $source = BASE_DIR.'/webcam/image1.png';
 
    $now = DateTime::createFromFormat('U.u', microtime(true));
    $now->setTimezone(new DateTimeZone('Europe/Amsterdam'));	
    $destination = $path.'/image1-'.$now->format("His.u").'.jpg';
 	
-   $im = imagecreatefromjpeg($source);
+   #$im = imagecreatefromjpeg($source);
+	$im = imagecreatefrompng($source);
+	
+	$textcolor = imagecolorallocate($im, 255, 255, 255);
+	imagestring($im, 5, 5, $height-20, $name, $textcolor);
+	imagestring($im, 5, ($width-75), $height-20, date("H:m:s"), $textcolor);
+	
    imagejpeg($im, $destination);	
 }
 
-function plaatprotect_motion($resolution) {
+function plaatprotect_motion($resolution, $name) {
 
    global $im2;
    global $index;
@@ -73,21 +80,23 @@ function plaatprotect_motion($resolution) {
    global $detect_areas;
 
    $width=320;
-   $height=225;  /* remove last 15 pixel lines because of footer */
+   $height=240;  /* remove last 15 pixel lines because of footer */
    $segment=5;
 		
    if ($resolution=="640x480") {
      $width=640;
-     $height=450;  /* remove last 15 pixel lines because of footer */
+     $height=480;  /* remove last 15 pixel lines because of footer */
      $segment=10;
    }
 	
    $offset=$segment/2;
 
-   $input = BASE_DIR.'/webcam/image'.$index.'.jpg';
+   #$input = BASE_DIR.'/webcam/image'.$index.'.jpg';
+	$input = BASE_DIR.'/webcam/image'.$index.'.png';
    $output = BASE_DIR.'/webcam/image'.($index+2).'.jpg';
 
-   $im1 = imagecreatefromjpeg($input);
+   #$im1 = imagecreatefromjpeg($input);
+	$im1 = imagecreatefrompng($input);
    if(!$im1) {
       return;
    }
@@ -102,22 +111,26 @@ function plaatprotect_motion($resolution) {
    $detection=0;
 
    for ($x=0;$x<($width/$segment);$x++) {
-     for ($y=0;$y<($height/$segment);$y++) {
+     for ($y=0;$y<(($height-20)/$segment);$y++) {
        list($x1, $y1, $r1, $g1, $b1) = getColor($im1, ($x*$segment)+$offset, ($y*$segment)+$offset);
        list($x2, $y2, $r2, $g2, $b2) = getColor($im2, ($x*$segment)+$offset, ($y*$segment)+$offset);
 
        if ((abs($r1-$r2)>$detect_level) || (abs($g1-$g2)>$detect_level) || (abs($b1-$b2)>$detect_level)) { 
           $detection++;
-	  imagerectangle( $im1, $x*$segment , $y*$segment , ($x+1)*$segment , ($y+1)*$segment , $color);
+	       imagerectangle( $im1, $x*$segment , $y*$segment , ($x+1)*$segment , ($y+1)*$segment , $color);
        }	
      }
    }
+	
+	$textcolor = imagecolorallocate($im1, 255, 255, 255);
+	imagestring($im1, 5, 5, $height-20, $name, $textcolor);
+	imagestring($im1, 5, ($width-75), $height-20, date("H:m:s"), $textcolor);
 
    $im2=$im1;
    imagejpeg($im1, $output);	
 
    if ($detection>$detect_areas) {
-     plaatprotect_make_picture();
+     plaatprotect_make_picture($name, $width, $height);
    }
 	
    return $detection;
@@ -139,11 +152,13 @@ while (true) {
   $device = plaatprotect_db_get_config_item('webcam_device', $instance);
   $webcam_fps = plaatprotect_db_get_config_item('webcam_fps', $instance);
 	 
-  $command = 'fswebcam -q --device '.$device.' --timestamp "%Y-%m-%d %H:%M:%S" -r '.$resolution. ' --title '.$name.' -S 1 '.BASE_DIR.'/webcam/image'.$index.'.jpg';
+  #$command = 'fswebcam -q --device '.$device.' --timesAtamp "%Y-%m-%d %H:%M:%S" -r '.$resolution. ' --title '.$name.' -S 1 '.BASE_DIR.'/webcam/image'.$index.'.jpg';
+  
+  $command = 'ffmpeg -i '.$device.' -s '.$resolution. ' -c png -frames 1 -v 0 -y '.BASE_DIR.'/webcam/image'.$index.'.png'; 
   exec($command);
   echo $command."\r\n";
 	
-  $detection_count = plaatprotect_motion($resolution);
+  $detection_count = plaatprotect_motion($resolution, $name);
 
   $time_end = microtime(true);	
   $time = ($time_end - $time_start)*1000000;
